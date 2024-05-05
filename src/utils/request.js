7b47bdf4c38f5da1b534a275399243f3be252c24
wcq <1514320887@qq.com>
2024-05-05 14:06:22 +0800

    过滤器版本

---
+++ src/utils/request.js
@@ -0,0 +1,37 @@
+import axios from 'axios'
+import { Message } from 'element-ui';
+// 创建axios实例，将来对创建出来的实例，进行自定义配置，不会污染原始的axios实例
+const instance = axios.create({
+  baseURL: 'http://localhost:8080',
+  timeout: 5000
+})
+
+
+// 添加请求拦截器
+instance.interceptors.request.use(function(config) {
+  // 在发送请求之前做些什么
+  return config;
+}, function(error) {
+  // 对请求错误做些什么
+  return Promise.reject(error);
+});
+
+// 添加响应拦截器
+instance.interceptors.response.use(function(response) {
+  const result = response.data
+  console.log(result);
+  if(result.code !== 200) {
+    Message({
+      type:'error',
+      message: result.msg
+    })
+  }
+  // 2xx 范围内的状态码都会触发该函数。
+  // 对响应数据做点什么
+  return Promise.reject(result.msg)
+}, function(error) {
+  // 超出 2xx 范围的状态码都会触发该函数。
+  // 对响应错误做点什么
+  return Promise.reject(error);
+});
+export default instance
